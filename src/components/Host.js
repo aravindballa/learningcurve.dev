7a401fc3533404bc7e689e086a7f8317842fe68f
Aravind Balla <bsaaravind@gmail.com>
2020-01-23 19:19:01 +0530

    fix: change unavatar to avataar

--- src/components/Host.js
+++ src/components/Host.js
@@ -5,7 +5,7 @@ const Host = ({ name, img, role, twitter }) => (
     <img
       alt={`${name} avatar`}
       className="host-avatar w-16 h-16 rounded-full mr-4 border-2 border-gray-100"
-      src={img || `https://unavatar.now.sh/${twitter}`}
+      src={img || `https://avatars.io/twitter/${twitter}`}
     />
     <div className="text-left">
       <p className="font-bold text-lg leading-none text-gray-700">{name}</p>
