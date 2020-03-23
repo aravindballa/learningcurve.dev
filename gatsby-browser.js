71e6e776c5245120c508c4c6cd01ade261bfae39
Aravind Balla <bsaaravind@gmail.com>
2020-03-23 15:23:56 +0530

    fix: layout typo

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import Layout from './src/components/layout';
+import Layout from './src/components/Layout';
 import './src/css/style.css';
 
 export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>;
 };
-
-// export const shouldUpdateScroll = () => {
-//   return false;
-// };
