003fa2e5ac314f6413fbf2c4a22c3d2d5d82c8b6
Aravind Balla <bsaaravind@gmail.com>
2020-04-02 20:54:03 +0530

    fix: episode art in safari

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,9 +10,9 @@ function Layout({ children }) {
   return (
     <MDXProvider components={mdxComps}>
       <div className="flex flex-col font-sans min-h-screen bg-gray-100 text-gray-900">
-        <main className="flex flex-col flex-1 md:justify-center max-w-4xl mx-auto px-4 py-8 md:p-8 w-full">
+        <div className="flex flex-col flex-1 md:justify-center max-w-4xl mx-auto px-4 py-8 md:p-8 w-full">
           {children}
-        </main>
+        </div>
         <Footer />
         <UpdateCacheModal />
       </div>
