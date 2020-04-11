6bbe1c9cc0c1cda17dc80acc2613e58e0a2fa280
Aravind Balla <bsaaravind@gmail.com>
2020-04-11 14:29:03 +0530

    feat: gh action runs on local files

--- scripts/getQuote.js
+++ scripts/getQuote.js
@@ -1,22 +1,43 @@
-const fetch = require('node-fetch');
-const cheerio = require('cheerio');
+const glob = require('glob');
+const path = require('path');
+const fs = require('fs');
+
+// const fetch = require('node-fetch');
+// const cheerio = require('cheerio');
 
 let tweet = '';
-const lastEpisode = 11;
+
+// const fetchTweetFromWebsite = async () => {
+//   const lastEpisode = 11;
+//   const url = `https://learningcurve.dev/${Math.floor(Math.random() * lastEpisode)}`;
+//   console.log('fetching -', url);
+//   const res = await fetch(url);
+//   const html = await res.text();
+//   const $ = cheerio.load(html);
+//   const quotes = $('blockquote')
+//     .map((i, el) => cheerio.load(el).text())
+//     .get();
+//   if (quotes.length) {
+//     tweet = quotes[Math.floor(Math.random() * quotes.length)];
+//   }
+// };
 
 (async () => {
+  const episodePages = glob.sync('*.md', { cwd: path.join(__dirname, '../episodes') });
   while (!tweet) {
-    const url = `https://learningcurve.dev/${Math.floor(Math.random() * lastEpisode)}`;
-    console.log('fetching -', url);
-    const res = await fetch(url);
-    const html = await res.text();
-    const $ = cheerio.load(html);
-    const quotes = $('blockquote')
-      .map((i, el) => cheerio.load(el).text())
-      .get();
-    if (quotes.length) {
-      tweet = quotes[Math.floor(Math.random() * quotes.length)];
-    }
+    const randomEpisode = fs.readFileSync(
+      path.join(
+        __dirname,
+        '../episodes',
+        episodePages[Math.floor(Math.random() * episodePages.length)]
+      ),
+      { encoding: 'utf-8' }
+    );
+    const quotes = randomEpisode
+      .split('\n')
+      .filter(line => line.match(/^>/))
+      .map(line => line.replace(/^>\s*/, ''));
+    tweet = quotes[Math.floor(Math.random() * quotes.length)];
   }
 
   console.log(tweet);
