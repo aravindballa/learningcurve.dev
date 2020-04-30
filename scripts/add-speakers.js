857366947cd23af992e63c02453ee9417d1c0c52
Aravind Balla <bsaaravind@gmail.com>
2020-04-30 17:17:55 +0530

    feat: better processing of transcripts

---
+++ scripts/add-speakers.js
@@ -0,0 +1,18 @@
+const glob = require('glob');
+const path = require('path');
+const fs = require('fs');
+
+const EPISODES_PATH = path.join(__dirname, '../episodes');
+
+(async () => {
+  const episodePages = glob.sync('*.md', { cwd: EPISODES_PATH });
+  for (const episodePage of episodePages) {
+    let episodeContent = fs.readFileSync(path.join(EPISODES_PATH, episodePage), {
+      encoding: 'utf-8',
+    });
+    episodeContent = episodeContent.replace(/\n(.*?)\s([0-9]+:[0-9]+)\s*/g, (_, name, time) => {
+      return `\n<TranscriptSpeaker name="${name}" time="${time}" />\n\n`;
+    });
+    fs.writeFileSync(path.join(EPISODES_PATH, episodePage), episodeContent, { encoding: 'utf-8' });
+  }
+})();
--- src/components/MDXComponents.js
+++ src/components/MDXComponents.js
@@ -7,82 +7,24 @@ const twitterHandle = {
   Brittik: 'brittikbasu',
 };
 
-const Paragraph = ({ children, ...rest }) => {
-  console.log(children);
-  if (Array.isArray(children)) {
-    const [speakerString, ...restChildren] = children;
-    const contentToDisplay = restChildren.filter((c) => {
-      if (typeof c === 'string') return !!c.trim();
-      return c.props.mdxType !== 'br';
-    });
-    const speakerMatch = speakerString.match(/(.*?)\s([0-9]+:[0-9]+)\s*/);
-    if (speakerMatch) {
-      return (
-        <div className="mt-4">
-          <div className="flex items-center">
-            <img
-              alt={speakerMatch[1]}
-              className="w-8 rounded-full mr-2"
-              src={`https://avatars.io/twitter/${twitterHandle[speakerMatch[1]]}`}
-            />
-            <div className="leading-tight">
-              <p className="text-gray-900 text-sm">
-                <strong>{speakerMatch[1]}</strong>
-              </p>
-              <p className="text-gray-700">
-                <span className="text-xs">{speakerMatch[2]}</span>
-              </p>
-            </div>
-          </div>
-          <div className="mt-1">
-            <p className="text-gray-900" {...rest} children={contentToDisplay} />
-          </div>
-        </div>
-      );
-    }
-  }
-  if (typeof children === 'string') {
-    const speakerMatch = children.match(/(.*?)\s([0-9]+:[0-9]+)\s*/);
-    if (speakerMatch) {
-      return (
-        <div className="mt-4">
-          <div className="flex items-center">
-            <img
-              alt={speakerMatch[1]}
-              className="w-8 rounded-full mr-2"
-              src={`https://avatars.io/twitter/${twitterHandle[speakerMatch[1]]}`}
-            />
-            <div className="leading-tight">
-              <p className="text-gray-900 text-sm">
-                <strong>{speakerMatch[1]}</strong>
-              </p>
-              <p className="text-gray-700">
-                <span className="text-xs">{speakerMatch[2]}</span>
-              </p>
-            </div>
-          </div>
-          <div className="mt-1">
-            <p
-              className="text-gray-900"
-              {...rest}
-              children={children.replace(speakerMatch[0], '')}
-            />
-          </div>
-        </div>
-      );
-    }
-  }
-  return <p className="mt-2" {...rest} children={children} />;
-};
-
-const BlockQuote = (props) => (
-  <blockquote
-    className="text-xl p-2 pb-4 pl-8 border-l-2 rounded font-serif italic mt-2 bg-gray-100"
-    {...props}
-  />
+const TranscriptSpeaker = ({ name, time }) => (
+  <div className="flex items-center mb-1">
+    <img
+      alt={name}
+      className="w-8 rounded-full mr-2"
+      src={`https://avatars.io/twitter/${twitterHandle[name]}`}
+    />
+    <div className="leading-tight">
+      <p className="text-gray-900 text-sm m-0">
+        <strong>{name}</strong>
+      </p>
+      <p className="text-gray-700 m-0">
+        <span className="text-xs">{time}</span>
+      </p>
+    </div>
+  </div>
 );
 
 export default {
-  p: Paragraph,
-  blockquote: BlockQuote,
+  TranscriptSpeaker,
 };
