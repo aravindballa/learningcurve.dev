8ebe78d802881bf196de8ca94e132873c1218c91
Aravind Balla <bsaaravind@gmail.com>
2020-06-25 18:46:12 +0530

    fix: title in social images

--- src/templates/EpisodePage.js
+++ src/templates/EpisodePage.js
@@ -30,12 +30,18 @@ const EpisodePage = ({ pageContext, data }) => {
     year: `numeric`,
   });
   const durationString = prettyMilliseconds(duration * 1000);
-  const ogImage = `https://res.cloudinary.com/djeivq7td/image/upload/w_1200,h_630,c_fill,q_auto,f_auto/w_857,c_fit,co_rgb:000000,g_north_west,x_108,y_87,l_text:Raleway_72_bold:${episode}${title
-    .replace('.', ' -')
-    .replace('?', '')
-    .replace(
-      /\p{Emoji}/gu,
-      ''
+  const ogImage = `https://res.cloudinary.com/djeivq7td/image/upload/w_1200,h_630,c_fill,q_auto,f_auto/w_857,c_fit,co_rgb:000000,g_north_west,x_108,y_87,l_text:Raleway_72_bold:
+    ${encodeURI(
+      episode +
+        '. ' +
+        title
+          .replace('.', ' -')
+          .replace('?', '')
+          .replace(
+            /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
+            ''
+          )
+          .trim()
     )}/w_857,c_fit,co_rgb:000000,g_south_west,x_140,y_180,l_text:Raleway_36:${dateString.replace(
     ',',
     ''
